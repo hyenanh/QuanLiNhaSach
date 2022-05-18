8f0c4d6dc4b8ba3a0cd0200942c2f2f6827762a2
quangtuan-0504.github.io <79434447+quangtuan-0504@users.noreply.github.com>
2022-05-18 13:42:57 +0700

    Add files via upload

---
+++ fPhieuThuTien.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace qliNhasach
+{
+    partial class fPhieuThuTien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSoTienThu = new System.Windows.Forms.TextBox();
+            this.btnThemPhieu = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.dgvHienThiThongTinKH = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnTroVe = new System.Windows.Forms.Button();
+            this.txtNgayThuTien = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHienThiThongTinKH)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(338, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phiếu thu tiền";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(59, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tìm kiếm khách hàng (nhập số điện thoại):";
+            // 
+            // btnTim
+            // 
+            this.btnTim.Location = new System.Drawing.Point(655, 60);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(79, 22);
+            this.btnTim.TabIndex = 2;
+            this.btnTim.Text = "Tìm";
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(342, 60);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(307, 22);
+            this.txtTimKiem.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(59, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ngày thu tiền";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(59, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Số tiền thu";
+            // 
+            // txtSoTienThu
+            // 
+            this.txtSoTienThu.Location = new System.Drawing.Point(161, 335);
+            this.txtSoTienThu.Name = "txtSoTienThu";
+            this.txtSoTienThu.Size = new System.Drawing.Size(200, 22);
+            this.txtSoTienThu.TabIndex = 3;
+            // 
+            // btnThemPhieu
+            // 
+            this.btnThemPhieu.Location = new System.Drawing.Point(399, 335);
+            this.btnThemPhieu.Name = "btnThemPhieu";
+            this.btnThemPhieu.Size = new System.Drawing.Size(116, 26);
+            this.btnThemPhieu.TabIndex = 7;
+            this.btnThemPhieu.Text = "Thêm phiếu";
+            this.btnThemPhieu.UseVisualStyleBackColor = true;
+            this.btnThemPhieu.Click += new System.EventHandler(this.btnThemPhieu_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(655, 386);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(79, 30);
+            this.btnThoat.TabIndex = 8;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // dgvHienThiThongTinKH
+            // 
+            this.dgvHienThiThongTinKH.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvHienThiThongTinKH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHienThiThongTinKH.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dgvHienThiThongTinKH.Location = new System.Drawing.Point(62, 110);
+            this.dgvHienThiThongTinKH.Name = "dgvHienThiThongTinKH";
+            this.dgvHienThiThongTinKH.RowHeadersWidth = 51;
+            this.dgvHienThiThongTinKH.RowTemplate.Height = 24;
+            this.dgvHienThiThongTinKH.Size = new System.Drawing.Size(690, 128);
+            this.dgvHienThiThongTinKH.TabIndex = 9;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Mã";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Họ tên";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 200;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Ngày sinh";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Địa chỉ";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Email";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Số điện thoại";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 150;
+            // 
+            // btnTroVe
+            // 
+            this.btnTroVe.Location = new System.Drawing.Point(546, 386);
+            this.btnTroVe.Name = "btnTroVe";
+            this.btnTroVe.Size = new System.Drawing.Size(82, 30);
+            this.btnTroVe.TabIndex = 10;
+            this.btnTroVe.Text = "Trở về";
+            this.btnTroVe.UseVisualStyleBackColor = true;
+            this.btnTroVe.Click += new System.EventHandler(this.btnTroVe_Click);
+            // 
+            // txtNgayThuTien
+            // 
+            this.txtNgayThuTien.Location = new System.Drawing.Point(161, 274);
+            this.txtNgayThuTien.Name = "txtNgayThuTien";
+            this.txtNgayThuTien.Size = new System.Drawing.Size(200, 22);
+            this.txtNgayThuTien.TabIndex = 11;
+            // 
+            // fPhieuThuTien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::qliNhasach.Properties.Resources._360_F_312397087_uJNHNhuzTKlz3kcMNhDL4Lpq23MypEoZ;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(826, 489);
+            this.Controls.Add(this.txtNgayThuTien);
+            this.Controls.Add(this.btnTroVe);
+            this.Controls.Add(this.dgvHienThiThongTinKH);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnThemPhieu);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSoTienThu);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.btnTim);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "fPhieuThuTien";
+            this.Text = "Phiếu thu tiền";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.fPhieuThuTien_FormClosing);
+            this.Load += new System.EventHandler(this.fPhieuThuTien_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHienThiThongTinKH)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSoTienThu;
+        private System.Windows.Forms.Button btnThemPhieu;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.DataGridView dgvHienThiThongTinKH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.Button btnTroVe;
+        private System.Windows.Forms.TextBox txtNgayThuTien;
+    }
+}
---
+++ fPhieuThuTien.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qliNhasach
+{
+    public partial class fPhieuThuTien : Form
+    {
+        public fPhieuThuTien()
+        {
+            InitializeComponent();
+        }
+
+        private void fPhieuThuTien_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThemPhieu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTroVe_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fPhieuThuTien_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult ret = MessageBox.Show("Bạn chắc chắn muốn thoát", "Thoát"
+                , MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (ret == DialogResult.Yes)
+            {
+                e.Cancel = false;// cancel la ko thoat giao dien
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
